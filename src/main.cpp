00209e5a8df73941ad793819e9a71a3e11ae1ad8
Trokkin <trokkin@gmail.com>
2021-05-19 21:33:03 +0300

    add gauss

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,52 @@
 ﻿#include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Work in progress ฅ^•ﻌ•^ฅ" << std::endl;
+using std::vector;
+
+vector<double> gauss(vector<int> b, vector<vector<int>> c) {
+    int n = b.size();
+    vector<vector<double>> arr(n, vector<double>(n + 1));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (j + 1 <= n)
+                arr[i][j] = c[i][j];
+            else
+                arr[i][j] = b[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if (arr[j][i] != 0) {
+                swap(arr[i], arr[j]);
+            }
+        }
+        double tmp = arr[i][i];
+        if (!tmp)
+            continue;
+        for (int j = n; j >= i; j--)
+            arr[i][j] /= tmp;
+        for (int j = i + 1; j < n; j++) {
+            tmp = arr[j][i];
+            for (int k = n; k >= i; k--)
+                arr[j][k] -= tmp * arr[i][k];
+        }
+    }
+    vector<double> ans(n);
+    ans[n - 1] = arr[n - 1][n];
+    for (int i = n - 2; i >= 0; i--) {
+        double sum = arr[i][n];
+        for (int j = i + 1; j < n; j++)
+            sum -= arr[i][j] * ans[j];
+        ans[i] = sum;
+    }
+    return ans;
+}
+
+int main() {
+    vector<double> ans = gauss({4}, {{2}});
+    for (int i = 0; i < ans.size(); i++)
+        std::cout << (int)ans[i] << " ";
 
     return 0;
 }
